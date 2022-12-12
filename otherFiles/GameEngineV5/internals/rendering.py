060ccb672a38b2cc2292f964ceed2146d8c1f5e6
Onni Kolkka <kolkka.onni@gmail.com>
2022-12-13 01:33:30 +0200

    Implemented matrices

--- otherFiles/GameEngineV5/internals/rendering.py
+++ otherFiles/GameEngineV5/internals/rendering.py
@@ -6,16 +6,26 @@ import sys
 from tkinter import *
 from dataclasses import dataclass
 import math
-from GameEngineV5.mathAndStructs import*
+from GameEngineV5.mathAndStructs import *
+
 if __name__ == "some random stuff to trick pep8 thinking that i have mathAndStructs module imported (I do)":
     from ..mathAndStructs import *
 
+
 @dataclass
 class draw_buffer:
-    triangles=[]
+    triangles = []
 
-    def __init__(self,tris=[]):
-        self.triangles=tris
+    def __init__(self, tris=[]):
+        self.triangles = tris
 
 
+def backface_culling(view_dir: Vector3, triangles: list):
+    filter(lambda t: t.normal * view_dir > 0, triangles)
 
+
+def frustum_culling(triangles: list):
+    filter_func = lambda triangle: (-1 < triangle.vert1.x < 1 or -1 < triangle.vert1.y < 1 or 0 < triangle.vert1.z < 1) \
+                             and (-1 < triangle.vert2.x < 1 or -1 < triangle.vert2.y < 1 or 0 < triangle.vert2.z < 1) \
+                             and (-1 < triangle.vert3.x < 1 or -1 < triangle.vert3.y < 1 or 0 < triangle.vert3.z < 1)
+    filter(filter_func, triangles)
--- otherFiles/GameEngineV5/mathAndStructs/vectors.py
+++ otherFiles/GameEngineV5/mathAndStructs/vectors.py
@@ -11,6 +11,8 @@ so added comments would only make reading harder for the most part"""
 from dataclasses import dataclass
 import unittest
 import math
+import logging
+
 
 
 # region vect
@@ -21,7 +23,7 @@ class Vector2:
         self.x = x
         self.y = y
 
-    def __eq__(self, other):
+    def __eq__(self, other)->bool:
         # guard clause for different data type
         if type(other) != type(self):
             return False
@@ -33,6 +35,17 @@ class Vector2:
             return False
 
 
+    def __mul__(self, other):
+        if type(other) is Vector2:
+            return float(self.x * other.x + self.y * other.y)
+        elif type(other) is float:
+            return Vector2(
+                self.x * other,
+                self.y * other
+            )
+        else:
+            raise TypeError
+
 @dataclass
 class Vector3:
 
@@ -41,7 +54,7 @@ class Vector3:
         self.y = y
         self.z = z
 
-    def __eq__(self, other):
+    def __eq__(self, other)->bool:
         # guard clause for different data type
         if type(other) != type(self):
             return False
@@ -54,6 +67,15 @@ class Vector3:
         else:
             return False
 
+    def __mul__(self, other):
+        if type(other) is Vector3:
+            return float(self.x * other.x + self.y * other.y + self.z * other.z)
+        elif type(other) is float:
+            self.x *= other
+            self.y *= other
+            self.z *= other
+        else:
+            raise TypeError
 
 @dataclass
 class Vector4:
@@ -64,7 +86,7 @@ class Vector4:
         self.z = z
         self.w = w
 
-    def __eq__(self, other):
+    def __eq__(self, other)->bool:
         # guard clause for different data type
         if type(other) != type(self):
             return False
@@ -77,6 +99,20 @@ class Vector4:
 
         else:
             return False
+
+    def __mul__(self, other):
+        if type(other) is Matrix4x4:
+            # as multiplication order doesn't matter, let's just let matrices handle this
+            return other*self
+        elif type(other) is float:
+            self.x *= other
+            self.y *= other
+            self.z *= other
+            self.w *= other
+        elif type(other) is Vector4:
+            return float(self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w)
+        else:
+            raise TypeError
 
 
 # endregion
@@ -102,7 +138,7 @@ class Triangle2d:
                self.vert2.x, self.vert2.y, \
                self.vert3.x, self.vert3.y
 
-    def __eq__(self, other):
+    def __eq__(self, other)->bool:
         # guard clause for different data type
         if type(other) != type(self):
             return False
@@ -133,7 +169,7 @@ class Triangle:
         self.normal = normal
         self.depth = depth
 
-    def __eq__(self, other):
+    def __eq__(self, other)->bool:
         # guard clause for different data type
         if type(other) != type(self):
             return False
@@ -155,32 +191,21 @@ class Triangle:
 def dot(a, b) -> float:
     """Dot product of any two vectors
     (of same type)"""
-
-    # guard clause for mismatch type
-    if type(a) != type(b):
-        raise TypeError
-
-    # apparently this was retuning ints, so now they are cast into floats
-    if type(a) == Vector2:
-        return float(_dot2(a, b))
-    if type(a) == Vector3:
-        return float(_dot3(a, b))
-    if type(a) == Vector4:
-        return float(_dot4(a, b))
-    else:
-        raise TypeError
-
-
-def _dot2(a: Vector2, b: Vector2) -> float:
-    return a.x * b.x + a.y * b.y
-
-
-def _dot3(a: Vector3, b: Vector3) -> float:
-    return a.x * b.x + a.y * b.y + a.z * b.z
-
-
-def _dot4(a: Vector4, b: Vector4) -> float:
-    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w
+    return a*b
+    #
+    # # guard clause for mismatch type
+    # if type(a) != type(b):
+    #     raise TypeError
+    #
+    # # apparently this was retuning ints, so now they are cast into floats
+    # if type(a) == Vector2:
+    #     return float(_dot2(a, b))
+    # if type(a) == Vector3:
+    #     return float(_dot3(a, b))
+    # if type(a) == Vector4:
+    #     return float(_dot4(a, b))
+    # else:
+    #     raise TypeError
 
 
 def cross(a: Vector3, b: Vector3) -> Vector3:
@@ -205,25 +230,28 @@ def rot(vector_to_rotate, *args):
         angle = args[2]
         axis_vector = args[1]
     else:
-        print(vector_to_rotate,args[1])
+        print(vector_to_rotate, args[1])
         raise TypeError
 
     if type(args[-1]) == bool:
         angle = math.radians(angle) if args[-1] else angle
-    if not hasattr("","axis_vector"):
-        return _rot2d(vector_to_rotate,angle)
+    if not hasattr("", "axis_vector"):
+        return _rot2d(vector_to_rotate, angle)
     else:
-        return _rot3d()#TODO: not implemented
+        return _rot3d()  # TODO: not implemented
 
-def _rot2d(vector,angle):
+
+def _rot2d(vector, angle):
     # https://en.wikipedia.org/wiki/Rotation_matrix
     x = vector.x * math.cos(angle) - vector.y * math.sin(angle)
     y = vector.x * math.sin(angle) + vector.y * math.cos(angle)
-    return Vector2(x,y)
+    return Vector2(x, y)
 
-def _rot3d(vector_to_rot,axis_vector,angle):
-    # TODO: can you figuer out what is missing? :-)
+
+def _rot3d(vector_to_rot, axis_vector, angle):
+    # TODO: can you figure out what is missing? :-)
     return vector_to_rot
+
 
 # endregion
 
@@ -245,17 +273,139 @@ class vectors_unit_test(unittest.TestCase):
         test(a4, b4)
 
         test = lambda a, b: self.assertIs(type(cross(a, b)), Vector3) \
-                        and self.assertIs(type(cross(a, a)), Vector3(0,0)) \
-                        and self.assertIs(type(cross(b, b)), Vector3(0,0))
+                            and self.assertIs(type(cross(a, a)), Vector3(0, 0)) \
+                            and self.assertIs(type(cross(b, b)), Vector3(0, 0))
 
         test(a3, b3)
         test(b3, a3)
 
         test = lambda a, b, rad=False: self.assertIs(type(rot(a, a, rad)), Vector2)
 
-        test(a2,math.pi,True)
-        test(a2,90)
-        test(b2,math.pi*3,True)
-        test(b2,75)
+        test(a2, math.pi, True)
+        test(a2, 90)
+        test(b2, math.pi * 3, True)
+        test(b2, 75)
+
+# endregion
+
+
+"""[insert short description here]"""
+"""Onni Kolkka 
+150832953 (student number)
+created 12.12.2022 20.31
+"""
+
+"""I believe this code is really self explanatory
+so added comments would only make reading harder for the most part"""
+
+
+# region matrix
+
+@dataclass
+class Matrix4x4:
+    def __init__(self, *args, suppress=False, eye=False):
+
+        if eye:
+            for i in range(4 * 4):
+                # creates eye matrix
+                setattr(self, f"m{i % 4}{int(i / 4)}", 1.0 if int(i / 4) == i % 4 else 0.0)
+            return
+
+        elif len(args) != 4 * 4:
+            if not suppress:
+                if len(args) > 0:
+                    raise Exception("\33[31mCreating Matrix with too few values "
+                                    "\33[33m(filled rest of the matrix with 0.0)\33[0m")
+
+            # fills matrix so it contains every elements
+            args += tuple([0.0] * (4 * 4 - len(args)))
+
+        for number, value in enumerate(args, start=0):
+            setattr(self, f"m{number % 4}{int(number / 4)}", value)
+            # this result in attributes like   matrix.m10  matrix.m03  matrix.m33
+
+    def __eq__(self, other) -> bool:
+        # guard clause for different data type
+        if type(other) != type(self):
+            return False
+
+        for number in range(4*4):
+            if getattr(self, f"m{number % 4}{int(number / 4)}") != getattr(self, f"m{number % 4}{int(number / 4)}"):
+                return False
+        else:
+            return True
+
+
+    def m4x4_times_m4x4(first, second):
+        # as we only allow 4x4 matrices, every matrix multiplication is always possible
+        calc = lambda x, y, n: getattr(first, f"m{x}{n}") * getattr(second, f"m{n}{y}")
+
+        # writing out the for loop to maybe get extra performance. pythons performance is pretty poor anyway
+        value_list = [
+            calc(0, 0, 0) + calc(0, 0, 1) + calc(0, 0, 2) + calc(0, 0, 3),
+            calc(1, 0, 0) + calc(1, 0, 1) + calc(1, 0, 2) + calc(1, 0, 3),
+            calc(2, 0, 0) + calc(2, 0, 1) + calc(2, 0, 2) + calc(2, 0, 3),
+            calc(3, 0, 0) + calc(3, 0, 1) + calc(3, 0, 2) + calc(3, 0, 3),
+
+            calc(0, 1, 0) + calc(0, 1, 1) + calc(0, 1, 2) + calc(0, 1, 3),
+            calc(1, 1, 0) + calc(1, 1, 1) + calc(1, 1, 2) + calc(1, 1, 3),
+            calc(2, 1, 0) + calc(2, 1, 1) + calc(2, 1, 2) + calc(2, 1, 3),
+            calc(3, 1, 0) + calc(3, 1, 1) + calc(3, 1, 2) + calc(3, 1, 3),
+
+            calc(0, 2, 0) + calc(0, 2, 1) + calc(0, 2, 2) + calc(0, 2, 3),
+            calc(1, 2, 0) + calc(1, 2, 1) + calc(1, 2, 2) + calc(1, 2, 3),
+            calc(2, 2, 0) + calc(2, 2, 1) + calc(2, 2, 2) + calc(2, 2, 3),
+            calc(3, 2, 0) + calc(3, 2, 1) + calc(3, 2, 2) + calc(3, 2, 3),
+
+            calc(0, 3, 0) + calc(0, 3, 1) + calc(0, 3, 2) + calc(0, 3, 3),
+            calc(1, 3, 0) + calc(1, 3, 1) + calc(1, 3, 2) + calc(1, 3, 3),
+            calc(2, 3, 0) + calc(2, 3, 1) + calc(2, 3, 2) + calc(2, 3, 3),
+            calc(3, 3, 0) + calc(3, 3, 1) + calc(3, 3, 2) + calc(3, 3, 3)
+        ]
+
+        return Matrix4x4(*value_list)
+
+    def m4x4_times_v4(first, second: Vector4) -> Vector4:
+        # as we only allow 4x4 matrices, every matrix multiplication is always possible
+        calc = lambda x, vector_value, n: getattr(first, f"m{x}{n}") * vector_value
+
+        # writing out the for loop to maybe get extra performance. pythons performance is pretty poor anyway
+        value_list = [
+            calc(0, second.x, 0) + calc(0, second.x, 1) + calc(0, second.x, 2) + calc(0, second.x, 3),
+            calc(1, second.y, 0) + calc(1, second.y, 1) + calc(1, second.y, 2) + calc(1, second.y, 3),
+            calc(2, second.z, 0) + calc(2, second.z, 1) + calc(2, second.z, 2) + calc(2, second.z, 3),
+            calc(3, second.w, 0) + calc(3, second.w, 1) + calc(3, second.w, 2) + calc(3, second.w, 3)
+        ]
+
+        return Vector4(*value_list)
+
+    def __mul__(self, other):
+        if type(other) == Matrix4x4:
+            return self.m4x4_times_m4x4(other)
+        elif type(other) == Vector4:
+            return self.m4x4_times_v4(other)
+        else:
+            raise TypeError
+
+# endregion
+
+# region uTest
+class matrices_unit_test(unittest.TestCase):
+    def test_dot(self):
+        self.assertIs(type(Matrix4x4(suppress=True)), Matrix4x4)
+        self.assertIs(type(Matrix4x4(eye=True)), Matrix4x4)
+
+        # too few args should throw error
+        self.assertRaises(Exception, Matrix4x4, 1)
+
+        # matrices can only be multiplied with vector4
+        test_func = lambda vector: Matrix4x4(eye=True) * vector
+        self.assertRaises(TypeError, test_func, Vector2(1, 1))
+        self.assertRaises(TypeError, test_func, Vector3(1, 1, 1))
+
+        # testing with eye matrix
+        self.assertEqual(test_func(Vector4(1, 1, 1, 1)), Vector4(1, 1, 1, 1))
+        self.assertEqual(test_func(Matrix4x4(1,1,1,1,1,1,suppress=True)), Matrix4x4(1,1,1,1,1,1,suppress=True))
+
 
 # endregion
