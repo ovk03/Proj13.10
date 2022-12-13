1c9418f5a51bfcde40ebeb17ca347bffdd133f07
Onni Kolkka <kolkka.onni@gmail.com>
2022-12-14 01:36:24 +0200

    preparing for testing optimization

--- otherFiles/__test__.py
+++ otherFiles/__test__.py
@@ -1,5 +1,4 @@
 """this file implements unittests"""
-import math
 
 """Onni Kolkka 
 150832953 (student number)
@@ -9,8 +8,11 @@ created 11.12.2022 20.53
 # import game engine from the game engine itself, on order to run unittests
 from GameEngineV6 import *
 import time
+import math
+import pstats
 import logging
 import unittest
+import cProfile
 
 def tri(pos):
     return Triangle(
@@ -65,10 +67,12 @@ def test():
 
         print(len(grid_test()))
         inter.render(DrawBuffer(grid_test()))
-        time.sleep(2)
+        t=0
         while inter.render():
             inter.camera_rot=Vector3(math.cos(i/3.3)*6,math.sin(i/7)*6,0)
-            i+=1
+            print(f"frame rate: {1 / (t + time.time()+1e-20)}")
+            t = -time.time()
+            i += 1
     except Exception as e:
         logging.getLogger().exception(e)
     unittest.main()
@@ -76,6 +80,8 @@ def test():
 
 if __name__ == "__main__":
     pass
+cProfile.run("test()","testStats")
+p = pstats.Stats('testStats')
+p.strip_dirs().sort_stats(pstats.SortKey.TIME).print_stats()
 
-test()
 
